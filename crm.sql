702fea441841d9ff4ca87b0a9eef8894a63f426a
Chris Cappetta <36048795+ccappetta@users.noreply.github.com>
2018-09-25 12:36:43 -0600

    Update crm.sql

--- crm.sql
+++ crm.sql
@@ -1,107 +1,94 @@
-
--- Dumping structure for table public.account
 CREATE TABLE IF NOT EXISTS "account" (
-	"account_id" BIGINT NOT NULL DEFAULT nextval('"Account_Account-ID_seq"'::regclass) COMMENT E'',
-	"account_name" CHARACTER VARYING NULL DEFAULT NULL COMMENT E'',
+	"account_id" BIGSERIAL NOT NULL,
+	"account_name" CHARACTER VARYING,
 	PRIMARY KEY ("account_id")
 );
 
--- Data exporting was unselected.
--- Dumping structure for table public.contact
+
 CREATE TABLE IF NOT EXISTS "contact" (
-	"contact_id" BIGINT NOT NULL DEFAULT nextval('"Contact_Contact_ID_seq"'::regclass) COMMENT E'',
-	"contact_name" CHARACTER VARYING NULL DEFAULT NULL COMMENT E'',
-	"role" CHARACTER VARYING NULL DEFAULT NULL COMMENT E'',
+	"contact_id" BIGSERIAL NOT NULL,
+	"contact_name" CHARACTER VARYING,
+	"role" CHARACTER VARYING,
 	PRIMARY KEY ("contact_id")
 );
 
--- Data exporting was unselected.
--- Dumping structure for table public.implementation
+
 CREATE TABLE IF NOT EXISTS "implementation" (
-	"implementation_id" BIGINT NOT NULL DEFAULT nextval('"Implementation_Implementation_ID_seq"'::regclass) COMMENT E'',
-	"implementation_project_name" CHARACTER VARYING NULL DEFAULT NULL COMMENT E'',
-	"implementation_start_datetime" TIMESTAMP WITHOUT TIME ZONE NULL DEFAULT NULL COMMENT E'',
-	"implementation_due_date" TIMESTAMP WITHOUT TIME ZONE NULL DEFAULT NULL COMMENT E'',
-	"client_task_1" CHARACTER VARYING NULL DEFAULT NULL COMMENT E'',
-	"client_task_2" CHARACTER VARYING NULL DEFAULT NULL COMMENT E'',
-	"client_task_3" CHARACTER VARYING NULL DEFAULT NULL COMMENT E'',
-	"company_task_1" CHARACTER VARYING NULL DEFAULT NULL COMMENT E'',
-	"company_task_2" CHARACTER VARYING NULL DEFAULT NULL COMMENT E'',
-	"company_task_3" CHARACTER VARYING NULL DEFAULT NULL COMMENT E'',
-	"project_status" CHARACTER VARYING NULL DEFAULT NULL COMMENT E'',
-	"customer_contact" CHARACTER VARYING NULL DEFAULT NULL COMMENT E'',
-	"project_owner" CHARACTER VARYING NULL DEFAULT NULL COMMENT E'',
-	"implementation_opportunity" CHARACTER VARYING NULL DEFAULT NULL COMMENT E'',
-	"account" CHARACTER VARYING NULL DEFAULT NULL COMMENT E'',
-	"friendly_start_datestring" CHARACTER VARYING NULL DEFAULT NULL COMMENT E'',
-	"friendly_due_datestring" CHARACTER VARYING NULL DEFAULT NULL COMMENT E'',
-	"implementation_opportunity_id" INTEGER NULL DEFAULT NULL COMMENT E'',
-	"customer_contact_phone" CHARACTER VARYING NULL DEFAULT NULL COMMENT E'',
-	"customer_contact_email" CHARACTER VARYING NULL DEFAULT NULL COMMENT E'',
-	"implementation_flowuri" CHARACTER VARYING NULL DEFAULT NULL COMMENT E'',
-	"client_flowuri" CHARACTER VARYING NULL DEFAULT NULL COMMENT E'',
-	"implementation_delivery_datestring" CHARACTER VARYING NULL DEFAULT NULL COMMENT E'',
+	"implementation_id" BIGSERIAL NOT NULL,
+	"implementation_project_name" CHARACTER VARYING,
+	"implementation_start_datetime" TIMESTAMP WITHOUT TIME ZONE,
+	"implementation_due_date" TIMESTAMP WITHOUT TIME ZONE,
+	"client_task_1" CHARACTER VARYING,
+	"client_task_2" CHARACTER VARYING,
+	"client_task_3" CHARACTER VARYING,
+	"company_task_1" CHARACTER VARYING,
+	"company_task_2" CHARACTER VARYING,
+	"company_task_3" CHARACTER VARYING,
+	"project_status" CHARACTER VARYING,
+	"customer_contact" CHARACTER VARYING,
+	"project_owner" CHARACTER VARYING,
+	"implementation_opportunity" CHARACTER VARYING,
+	"account" CHARACTER VARYING,
+	"friendly_start_datestring" CHARACTER VARYING,
+	"friendly_due_datestring" CHARACTER VARYING,
+	"implementation_opportunity_id" INTEGER,
+	"customer_contact_phone" CHARACTER VARYING,
+	"customer_contact_email" CHARACTER VARYING,
+	"implementation_flowuri" CHARACTER VARYING,
+	"client_flowuri" CHARACTER VARYING,
+	"implementation_delivery_datestring" CHARACTER VARYING,
 	PRIMARY KEY ("implementation_id")
 );
 
--- Data exporting was unselected.
--- Dumping structure for table public.lead
+
 CREATE TABLE IF NOT EXISTS "lead" (
-	"lead_id" BIGINT NOT NULL DEFAULT nextval('"Lead_Lead_ID_seq"'::regclass) COMMENT E'',
-	"lead_nombre" CHARACTER VARYING NULL DEFAULT NULL COMMENT E'',
+	"lead_id" BIGSERIAL NOT NULL,
+	"lead_nombre" CHARACTER VARYING,
 	PRIMARY KEY ("lead_id")
 );
 
--- Data exporting was unselected.
--- Dumping structure for table public.opportunity
+
 CREATE TABLE IF NOT EXISTS "opportunity" (
-	"opportunity_id" BIGINT NOT NULL DEFAULT nextval('"Opportunity_Opportunity_ID_seq"'::regclass) COMMENT E'',
-	"opportunity_name" CHARACTER VARYING NULL DEFAULT NULL COMMENT E'',
-	"account_id" INTEGER NULL DEFAULT NULL COMMENT E'',
-	"account_name" CHARACTER VARYING NULL DEFAULT NULL COMMENT E'',
-	"description" CHARACTER VARYING NULL DEFAULT NULL COMMENT E'',
-	"stage" CHARACTER VARYING NULL DEFAULT NULL COMMENT E'',
-	"probability" INTEGER NULL DEFAULT NULL COMMENT E'',
-	"close_date" TIMESTAMP WITHOUT TIME ZONE NULL DEFAULT NULL COMMENT E'',
-	"opportunity_type" CHARACTER VARYING NULL DEFAULT NULL COMMENT E'',
-	"next_step" CHARACTER VARYING NULL DEFAULT NULL COMMENT E'',
-	"lead_source" CHARACTER VARYING NULL DEFAULT NULL COMMENT E'',
-	"is_closed" BOOLEAN NULL DEFAULT NULL COMMENT E'',
-	"is_won" BOOLEAN NULL DEFAULT NULL COMMENT E'',
-	"forecast_category" CHARACTER VARYING NULL DEFAULT NULL COMMENT E'',
-	"campaign_id" INTEGER NULL DEFAULT NULL COMMENT E'',
-	"pricebook_id" INTEGER NULL DEFAULT NULL COMMENT E'',
-	"owner_id" INTEGER NULL DEFAULT NULL COMMENT E'',
-	"owner_name" CHARACTER VARYING NULL DEFAULT NULL COMMENT E'',
-	"created_date" TIMESTAMP WITHOUT TIME ZONE NULL DEFAULT NULL COMMENT E'',
-	"created_by_id" INTEGER NULL DEFAULT NULL COMMENT E'',
-	"created_by_name" CHARACTER VARYING NULL DEFAULT NULL COMMENT E'',
-	"last_modified" TIMESTAMP WITHOUT TIME ZONE NULL DEFAULT NULL COMMENT E'',
-	"last_modified_by_id" INTEGER NULL DEFAULT NULL COMMENT E'',
-	"last_activity_datetime" TIMESTAMP WITHOUT TIME ZONE NULL DEFAULT NULL COMMENT E'',
-	"url" CHARACTER VARYING NULL DEFAULT NULL COMMENT E'',
-	"other1" CHARACTER VARYING NULL DEFAULT NULL COMMENT E'',
-	"implementation_id" INTEGER NULL DEFAULT NULL COMMENT E'',
-	"friendly_created_datestring" CHARACTER VARYING NULL DEFAULT NULL COMMENT E'',
-	"friendly_modified_datestring" CHARACTER VARYING NULL DEFAULT NULL COMMENT E'',
-	"friendly_last_action_datestring" CHARACTER VARYING NULL DEFAULT NULL COMMENT E'',
-	"friendly_close_date" CHARACTER VARYING NULL DEFAULT NULL COMMENT E'',
+	"opportunity_id" BIGSERIAL NOT NULL,
+	"opportunity_name" CHARACTER VARYING,
+	"account_id" INTEGER,
+	"account_name" CHARACTER VARYING,
+	"description" CHARACTER VARYING,
+	"stage" CHARACTER VARYING,
+	"probability" INTEGER,
+	"close_date" TIMESTAMP WITHOUT TIME ZONE,
+	"opportunity_type" CHARACTER VARYING,
+	"next_step" CHARACTER VARYING,
+	"lead_source" CHARACTER VARYING,
+	"is_closed" BOOLEAN,
+	"is_won" BOOLEAN,
+	"forecast_category" CHARACTER VARYING,
+	"campaign_id" INTEGER,
+	"pricebook_id" INTEGER,
+	"owner_id" INTEGER,
+	"owner_name" CHARACTER VARYING,
+	"created_date" TIMESTAMP WITHOUT TIME ZONE,
+	"created_by_id" INTEGER,
+	"created_by_name" CHARACTER VARYING,
+	"last_modified" TIMESTAMP WITHOUT TIME ZONE,
+	"last_modified_by_id" INTEGER,
+	"last_activity_datetime" TIMESTAMP WITHOUT TIME ZONE,
+	"url" CHARACTER VARYING,
+	"other1" CHARACTER VARYING,
+	"implementation_id" INTEGER,
+	"friendly_created_datestring" CHARACTER VARYING,
+	"friendly_modified_datestring" CHARACTER VARYING,
+	"friendly_last_action_datestring" CHARACTER VARYING,
+	"friendly_close_date" CHARACTER VARYING,
 	PRIMARY KEY ("opportunity_id")
 );
 
--- Data exporting was unselected.
--- Dumping structure for table public.product
 CREATE TABLE IF NOT EXISTS "product" (
-	"product_id" BIGINT NOT NULL DEFAULT nextval('"Product_Product_ID_seq"'::regclass) COMMENT E'',
-	"product_name" CHARACTER VARYING NULL DEFAULT NULL COMMENT E'',
-	"pricebook_id" INTEGER NULL DEFAULT NULL COMMENT E'',
-	"quantity" INTEGER NULL DEFAULT NULL COMMENT E'',
-	"price" INTEGER NULL DEFAULT NULL COMMENT E'',
-	"description" CHARACTER VARYING NULL DEFAULT NULL COMMENT E'',
+	"product_id" BIGSERIAL NOT NULL,
+	"product_name" CHARACTER VARYING,
+	"pricebook_id" INTEGER,
+	"quantity" INTEGER,
+	"price" INTEGER,
+	"description" CHARACTER VARYING,
 	PRIMARY KEY ("product_id")
 );
-
--- Data exporting was unselected.
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
