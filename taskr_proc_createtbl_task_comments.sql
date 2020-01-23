625d4a837d1b95496b9056f067adc26c8b839be5
Chris Cappetta <36048795+ccappetta@users.noreply.github.com>
2020-01-23 12:41:10 -0700

    Create taskr_proc_createtbl_task_comments.sql

---
+++ taskr_proc_createtbl_task_comments.sql
@@ -0,0 +1,16 @@
+DROP procedure if exists proc_createtbl_task_comments;
+create procedure proc_createtbl_task_comments(IN dbname varchar(128))
+BEGIN
+    SET @theSQL=CONCAT('CREATE TABLE ', dbname, '.task_comments
+(
+    comment_id   int auto_increment
+        primary key,
+    comment_body varchar(9999) null,
+    commenter    varchar(255)  null,
+    task_id      varchar(20)   null,
+    comment_time timestamp     null
+);');
+    PREPARE stmt1 from @theSQL;
+    EXECUTE stmt1;
+    DEALLOCATE PREPARE stmt1;
+END;
