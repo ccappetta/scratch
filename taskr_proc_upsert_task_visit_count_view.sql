8df0f4ee286f4b61d82e089134f54c57a179d9b0
Chris Cappetta <36048795+ccappetta@users.noreply.github.com>
2020-01-23 13:03:18 -0700

    Update taskr_proc_upsert_task_visit_count_view.sql

--- taskr_proc_upsert_task_visit_count_view.sql
+++ taskr_proc_upsert_task_visit_count_view.sql
@@ -2,10 +2,6 @@ DROP procedure if exists proc_upsert_task_visit_count_view;
 create procedure proc_upsert_task_visit_count_view(IN dbname varchar(128))
 BEGIN
     SET @theSQL=CONCAT('
-create or replace view ', dbname,'.task_types_view as select distinct ', dbname,'.task.task_type AS task_type
-from ', dbname,'.task
-where ((', dbname,'.task.task_type is not null) and (', dbname,'.task.task_type <> ''''));
-
 create or replace view ', dbname,'.task_visit_count_view as select `a`.`task_uuid`              AS `task_uuid`,
        `a`.`task_subject`           AS `task_subject`,
        count(`b`.`viewing_task_id`) AS `view_count`
