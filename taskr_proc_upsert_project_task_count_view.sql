4ffde8a8a1405f2d0c89833c97cf99aa20bf24ad
Chris Cappetta <36048795+ccappetta@users.noreply.github.com>
2020-01-30 09:11:36 -0700

    Update taskr_proc_upsert_project_task_count_view.sql

--- taskr_proc_upsert_project_task_count_view.sql
+++ taskr_proc_upsert_project_task_count_view.sql
@@ -2,7 +2,7 @@ DROP procedure if exists proc_upsert_project_task_count_view;
 create procedure proc_upsert_project_task_count_view(IN dbname varchar(128))
 BEGIN
     SET @theSQL=CONCAT('
-create or replace view taskr_one.project_task_count_view as select coalesce(nullif(`a`.`parent_project`, ''''), ''Unattached'')          AS `parent_project`,
+create or replace view ', dbname,'.project_task_count_view as select coalesce(nullif(`a`.`parent_project`, ''''), ''Unattached'')          AS `parent_project`,
        count(''parent_project'')                                           AS `total_project_tasks`,
        count((case when (`a`.`status_message` = ''Complete'') then 1 end)) AS `completed_project_tasks`
 from ', dbname,'.`task` `a`
