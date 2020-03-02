51f36fe7d29cd0109e61f8e4e1c1b198d2ff57dd
Chris Cappetta <36048795+ccappetta@users.noreply.github.com>
2020-03-02 16:18:32 -0700

    Create taskr_proc_upsert_sf_dollar_value_fy_q.sql

---
+++ taskr_proc_upsert_sf_dollar_value_fy_q.sql
@@ -0,0 +1,23 @@
+DROP procedure if exists proc_upsert_sf_dollar_value_fy_q;
+create procedure proc_upsert_sf_dollar_value_fy_q(IN dbname varchar(128))
+BEGIN
+    SET @theSQL=CONCAT('
+
+create or replace view ', dbname,'.sf_dollar_value_fy_q as select sum(`O`.`opp_acv`)             AS `opp_acv`,
+       sum(`O`.`opp_tcv`)             AS `opp_tcv`,
+       count(`O`.`opportunity_sf_id`) AS `opp_count`,
+       `O`.`opportunity_status`       AS `opp_stage`,
+       `O`.`is_closed`                AS `opp_is_closed`,
+       `O`.`close_date`               AS `opp_close_date`,
+       count(`T`.`task_id`)           AS `task_count`,
+       `O`.`close_date_quarter`       AS `closed_quarter`,
+       `O`.`close_date_fy`            AS `closed_fiscalyear`
+from (', dbname,'.`related_opportunity` `O`
+         join ', dbname,'.`task` `T`)
+where ((`O`.`task_id` = `T`.`task_uuid`) and (`O`.`is_closed` = ''true''))
+group by `O`.`close_date_fy`, `O`.`close_date_quarter`;
+');
+    PREPARE stmt1 from @theSQL;
+    EXECUTE stmt1;
+    DEALLOCATE PREPARE stmt1;
+END;
