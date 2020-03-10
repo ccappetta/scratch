43afb8d99d80da751ae69c24b9c4e8c1194bbc54
Chris Cappetta <36048795+ccappetta@users.noreply.github.com>
2020-03-10 15:38:42 -0600

    Create taskr_proc_upsert_view_opps_touched.sql

---
+++ taskr_proc_upsert_view_opps_touched.sql
@@ -0,0 +1,21 @@
+DROP procedure if exists proc_upsert_view_opps_touched;
+create procedure proc_upsert_view_opps_touched(IN dbname varchar(128))
+BEGIN
+    SET @theSQL=CONCAT('
+
+create or replace view ', dbname,'.view_opps_touched as select sum(`O`.`opp_acv`)             AS `opp_acv`,
+       sum(`O`.`opp_tcv`)             AS `opp_tcv`,
+       count(`O`.`opportunity_sf_id`) AS `opp_count`,
+       `O`.`opportunity_status`       AS `opp_stage`,
+       `O`.`is_closed`                AS `opp_is_closed`,
+       `O`.`task_count`               AS `task_count`
+from ', dbname,'.`distinct_opps` `O`
+group by `O`.`opportunity_status`;
+
+
+
+');
+    PREPARE stmt1 from @theSQL;
+    EXECUTE stmt1;
+    DEALLOCATE PREPARE stmt1;
+END;
