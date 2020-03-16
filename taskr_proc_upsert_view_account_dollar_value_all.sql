e00b78085a652f6cbfa4c74cdbe1ba0b28c49889
Chris Cappetta <36048795+ccappetta@users.noreply.github.com>
2020-03-16 15:26:53 -0600

    Update taskr_proc_upsert_view_account_dollar_value_all.sql

--- taskr_proc_upsert_view_account_dollar_value_all.sql
+++ taskr_proc_upsert_view_account_dollar_value_all.sql
@@ -3,18 +3,20 @@ create procedure proc_upsert_view_account_dollar_value_all(IN dbname varchar(128
 BEGIN
     SET @theSQL=CONCAT('
 
+
 create or replace view ', dbname,'.view_account_dollar_value_all as select sum(`B`.`account_acv`)     AS `account_acv`,
        sum(`B`.`account_tcv`)     AS `account_tcv`,
        count(`B`.`account_sf_id`) AS `account_count`,
-       count(`T`.`task_id`)       AS `task_count`
-from (((select sum(`A`.`account_acv`) AS `account_acv`,
-               sum(`A`.`account_tcv`) AS `account_tcv`,
-               `A`.`task_id`          AS `task_id`,
-               `A`.`account_sf_id`    AS `account_sf_id`
-        from ', dbname,'.`related_account` `A`
-        group by `A`.`task_id`, `A`.`account_sf_id`)) `B`
-         join ', dbname,'.`task` `T`)
-where (`B`.`task_id` = `T`.`task_uuid`);
+       sum(`B`.`task_id_count`)   AS `task_count`
+from ((select `A`.`account_acv`    AS `account_acv`,
+              `A`.`account_tcv`    AS `account_tcv`,
+              count(`A`.`task_id`) AS `task_id_count`,
+              `A`.`account_sf_id`  AS `account_sf_id`
+       from (', dbname,'.`related_account` `A`
+                join ', dbname,'.`task` `T`)
+       where (`A`.`task_id` = `T`.`task_uuid`)
+       group by `A`.`account_sf_id`)) `B`;
+
 
 ');
     PREPARE stmt1 from @theSQL;
